46fbf47eb600dc1e2daef909ff08ef739ec11dd0
Léo Legron <leo.legron@gmail.com>
2017-12-23 01:50:46 +0100

    init __

--- APICall/APICallController.swift
+++ APICall/APICallController.swift
@@ -12,25 +12,6 @@ class APICallController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let url = URL(string: "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyATUkqiZ6WQVbJdu1vqj-oM3aP7MgedOeU&q=nou")
-        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
-            if error != nil {
-                print("ERROR")
-            }
-            else{
-                if let content = data{
-                    do{
-                        let myJSON = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
-                        print(myJSON)
-                    }
-                    catch{
-                        
-                    }
-                }
-            }
-        }
-        task.resume()
 
         // Do any additional setup after loading the view.
     }
