e105b47ba7297f6a97f335e52dd4152551092b60
Léo Legron <leo.legron@gmail.com>
2017-12-23 01:55:29 +0100

    testing youtube API calls

--- APICall/APICallController.swift
+++ APICall/APICallController.swift
@@ -12,13 +12,29 @@ class APICallController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyATUkqiZ6WQVbJdu1vqj-oM3aP7MgedOeU&q=nou")
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            if error != nil {
+                print("ERROR")
+            }
+            else{
+                if let content = data{
+                    do{
+                        let myJSON = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        print(myJSON)
+                    }
+                    catch{
+                        
+                    }
+                }
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
