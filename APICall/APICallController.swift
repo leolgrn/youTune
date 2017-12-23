00dd23a09c6f82692f7f903253eadacb7cc139de
Léo Legron <leo.legron@gmail.com>
2017-12-23 01:48:37 +0100

    init _

---
+++ APICall/APICallController.swift
@@ -0,0 +1,43 @@
+//
+//  APICallController.swift
+//  APICall
+//
+//  Created by Léo LEGRON on 21/12/2017.
+//  Copyright © 2017 Léo LEGRON. All rights reserved.
+//
+
+import UIKit
+
+class APICallController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyATUkqiZ6WQVbJdu1vqj-oM3aP7MgedOeU&q=nou")
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            if error != nil {
+                print("ERROR")
+            }
+            else{
+                if let content = data{
+                    do{
+                        let myJSON = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        print(myJSON)
+                    }
+                    catch{
+                        
+                    }
+                }
+            }
+        }
+        task.resume()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
